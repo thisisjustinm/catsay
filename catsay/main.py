f30a0795baeb45f589d357bdacf5bc3ec7415e0e
Justin M <23431812+thisisjustinm@users.noreply.github.com>
2021-02-13 00:36:13 +0530

    Update main.py

--- catsay/main.py
+++ catsay/main.py
@@ -5,11 +5,10 @@ import urllib3
 from bs4 import BeautifulSoup
 from drawtable import Table
 # from termcolor import colored  #print(colored('hello', 'red'), colored('world', 'green'))
-from .animals import *
+from .animals import animals
 from .quotes import ret_random_quote
 nam = ''
 animal_choices_list = ['cat','dog','duck','camel','deer','dolphin','whale','snail','butterfly','hedgehog','fox','hippo','otter','rabbit','mouse']
-animal_choices_dic = {'cat': 'cat[0]', 'dog': 'dog[0]', 'duck': 'duck[0]', 'camel': 'camel[0]', 'deer': 'deer[0]', 'dolphin': 'dolphin[0]', 'whale': 'whale[0]', 'snail': 'snail[0]', 'butterfly': 'butterfly[0]', 'hedgehog': 'hedgehog[0]', 'fox': 'fox[0]', 'hippo': 'hippo[0]', 'otter': 'otter[0]', 'rabbit': 'rabbit[0]', 'mouse': 'mouse[0]'}
 
 tb = Table(margin_x=1, margin_y=1, align='left', max_col_width=40)
 parser = argparse.ArgumentParser(description='Make cats say stuff.')
@@ -32,10 +31,10 @@ else:
     full_quote = lolcatzify(quote['text']) + '\n\n\n'  # + (quote['author'])
 
 if args.animal in animal_choices_list:
-    args.animal = nam
-    animal = eval(animal_choices_dic[args.animal])
+    animal_choice = args.animal
+    animal = animals[animal_choice]
 else:
-    animal = cat[0]
+    animal = animals['cat]
 
 
 def main():
